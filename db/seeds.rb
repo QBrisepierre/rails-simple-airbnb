34ccdd76cf8d8d5d83daa25f76a5e28bdfcd0ef2
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-10-14 16:16:52 +0200

    index and show designed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy
+20.times do 
+  flat = Flat.new(
+    name: "Light & Spacious Garden Flat London",
+    address: Faker::Address.full_address,
+    description: "10 Clifton Gardens London W9 1DT",
+    price_per_night: Faker::Number.within(range: 30..250),
+    number_of_guests: Faker::Number.within(range: 1..5)
+  )
+  flat.save
+end
